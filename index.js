0f519c6f3d0a993bc4a337e437cae0143fbcb479
emilton <emiltonnhampossa0@gmail.com>
2023-03-21 10:55:53 -0700

    feat:create api

---
+++ index.js
@@ -0,0 +1,54 @@
+/*
+    dependecias
+*/
+const express = require('express')
+const admin = require('firebase-admin');
+
+
+/*
+    config - express
+*/
+const app = express()
+/*
+    config-firebase
+*/
+const serviceAccount = require('./serviceAccountKey');
+
+admin.initializeApp({
+  credential:admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+ 
+/*
+    endpoint
+*/
+
+app.get('/posts', (request, response) => {
+    response.set('Access-Control-Allow-Origin','*')
+    let posts = []
+     db.collection('posts').orderBy('date', 'desc').get().then(snapshot=>
+        
+        {
+        snapshot.forEach((doc)=>{
+           posts.push(doc.data())
+        });
+     response.send(posts)
+  })
+})
+
+/*
+    endpoint - createPosts
+*/
+
+app.get('/createPost', (request, response) => {
+    response.set('Access-Control-Allow-Origin','*')
+    response.send('createPost')
+})
+    
+/*
+    listen
+*/
+
+
+app.listen(process.env.PORT || 3000) 
